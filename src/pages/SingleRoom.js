bf8b6b301abab7589dcf0cc30f2ed42d0af0b04a
abdelkareemS <abdelkareem.saleh78@gmail.com>
2020-07-15 17:06:00 +0200

    Services Section Done !

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+    state = {}
+    render() {
+        return (
+            <div>Hello From Single Room Page</div>
+        );
+    }
+}
+
+export default SingleRoom;
